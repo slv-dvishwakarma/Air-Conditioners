e235b945d1bf99014343ac3f5b18db3dc1dc1736
slv-dvishwakarma <dvishwakarma@skilline.ai>
2024-05-08 12:54:19 +0530

    newspapper

---
+++ app/newspapper/page.tsx
@@ -0,0 +1,13 @@
+import { NewsPapper } from '@/components/NewsPapper'
+import { ParentContainer } from '@/components/ParentContainer'
+import React from 'react'
+
+const Page = () => {
+  return (
+    <ParentContainer>
+    <NewsPapper />
+    </ParentContainer>
+  )
+}
+
+export default Page
---
+++ components/NewsPapper/NewsPapper.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import React from 'react'
+
+export const NewsPapper = () => {
+  return (
+    // <div className='m-auto'><iframe src="https://d1csarkz8obe9u.cloudfront.net/index.php/posterbuilder/view/0ffc4169cadac79de860a5e0e59fb251/1" style={{height: "800px", width: "100%", margin: "auto"}}></iframe></div>
+<Image src="/0ffc4169cadac79de860a5e0e59fb251_embed.jpg" alt="ac" width={695} height={900} className='m-auto'/>
+)
+
+}
